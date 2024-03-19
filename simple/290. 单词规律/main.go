99683aedd2ac84a4cf6188c2b83ce2a21c1fd0ea
mengde <mengde@xiaoshouyi.com>
2024-03-19 09:22:00 +0800

    add

---
+++ simple/290. 单词规律/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strings"
+)
+
+func wordPattern(pattern string, s string) bool {
+	patternToWord := map[byte]string{}
+	wordToPattern := map[string]byte{}
+	sl := strings.Split(s, " ")
+	if len(pattern) != len(sl) {
+		return false
+	}
+	for i := range pattern {
+		if word, ok := patternToWord[pattern[i]]; ok {
+			if word != sl[i] {
+				return false
+			}
+		} else {
+			if p, pok := wordToPattern[sl[i]]; pok {
+				if p != pattern[i] {
+					return false
+				}
+			}
+			patternToWord[pattern[i]] = sl[i]
+			wordToPattern[sl[i]] = pattern[i]
+		}
+	}
+	return true
+}
+
+func main() {
+
+}
---
+++ simple/338. 比特位计数/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+//func countBits(n int) []int {
+//	res := make([]int, 0, n+1)
+//	for i := 0; i <= n; i++ {
+//		tmp := i
+//		sum := 0
+//		for tmp > 0 {
+//			if tmp%2 == 1 {
+//				sum += 1
+//			}
+//			tmp /= 2
+//		}
+//		res = append(res, sum)
+//	}
+//	return res
+//}
+
+//func onceCount(n int) int {
+//	sum := 0
+//	for ; n > 0; n &= n - 1 {
+//		sum += 1
+//	}
+//	return sum
+//}
+//
+//func countBits(n int) []int {
+//	res := make([]int, n+1)
+//	for x := range res {
+//		res[x] = onceCount(x)
+//	}
+//	return res
+//}
+
+// 方法二：动态规划——最高有效位
+func countBits(n int) []int {
+	bits := make([]int, n+1)
+	highBit := 0
+	for i := 1; i <= n; i++ {
+		if i&(i-1) == 0 {
+			highBit = i
+		}
+		bits[i] = bits[i-highBit] + 1
+	}
+	return bits
+}
+
+func main() {
+	fmt.Println(countBits(16))
+}
