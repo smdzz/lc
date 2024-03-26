24bf613eda8057f9d901a56dca96fa08d4ee9a13
mengde <mengde@xiaoshouyi.com>
2024-03-26 10:01:44 +0800

    add

---
+++ simple/441. 排列硬币/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+//func arrangeCoins(n int) int {
+//	lines := 0
+//	lineCount := 1
+//	for n > 0 {
+//		if n >= lineCount {
+//			lines += 1
+//		}
+//		n -= lineCount
+//		lineCount += 1
+//	}
+//	return lines
+//}
+
+// 二分查找
+func arrangeCoins(n int) int {
+	if n == 1 {
+		return 1
+	}
+	left, right := 1, n
+	for left < right {
+		middle := (left + right) / 2
+		if middle*(middle+1) <= 2*n {
+			left = middle + 1
+		} else {
+			right = middle
+		}
+	}
+	return left - 1
+}
+func main() {
+	fmt.Println(arrangeCoins(3))
+}
---
+++ simple/448. 找到所有数组中消失的数字/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+//	func findDisappearedNumbers(nums []int) []int {
+//		res := []int{}
+//		m := map[int]struct{}{}
+//		for _, v := range nums {
+//			m[v] = struct{}{}
+//		}
+//		for n := 1; n <= len(nums); n++ {
+//			if _, ok := m[n]; !ok {
+//				res = append(res, n)
+//			}
+//		}
+//		return res
+//	}
+func findDisappearedNumbers(nums []int) (ans []int) {
+	n := len(nums)
+	for _, v := range nums {
+		v = (v - 1) % n
+		nums[v] += n
+	}
+	for i, v := range nums {
+		if v <= n {
+			ans = append(ans, i+1)
+		}
+	}
+	return
+}
+
+func main() {
+	fmt.Println(findDisappearedNumbers([]int{4, 3, 2, 7, 8, 2, 3, 1}))
+}
